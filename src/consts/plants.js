67a3aa2d5637b169c14f6bbfcecd527241cc9bf5
Shreya211120 <70424098+Shreya211120@users.noreply.github.com>
2022-05-15 09:25:42 +0530

    Initial plant commit

---
+++ src/consts/plants.js
@@ -0,0 +1,81 @@
+const plants = [
+    {
+      id: 1,
+      name: 'Succulent Plant',
+      price: '40.99',
+      like: true,
+      img: require('../assets/plant1.png'),
+      about:
+        'In botany, succulent plants, also known as succulents, are plants with parts that are thickened, fleshy, and engorged, usually to retain water in arid climates or soil conditions.',
+    },
+  
+    {
+      id: 2,
+      name: 'Dragon Plant',
+      price: '45.99',
+      like: false,
+      img: require('../assets/plant2.png'),
+      about:
+        'Dracaena draco, the Canary Islands dragon tree or drago, is a subtropical tree in the genus Dracaena, native to the Canary Islands, Cape Verde, Madeira, western Morocco, and is thought to be introduced in the Azores. Its closest living relative is the dragon blood tree of Socotra, Dracaena cinnabari.',
+    },
+    {
+      id: 3,
+      name: 'Ravenea Plant',
+      price: '59.99',
+      like: false,
+      img: require('../assets/plant3.png'),
+      about:
+        'Ravenala is a genus of monocotyledonous flowering plants. Classically, the genus was considered to include a single species.  A travellers palm tree is mildly toxic to humans and animals. If you have a cat or dog and they mistakenly ingest a leaf from your palm tree, they are in big trouble and should be taken to an emergency vet asap.',
+    },
+  
+    {
+      id: 4,
+      name: 'Potted Plant',
+      price: '45.99',
+      like: true,
+      img: require('../assets/plant4.png'),
+      about:
+        'Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.',
+    },
+    {
+      id: 5,
+      name: 'Dracaena Plant',
+      price: '30.99',
+      like: true,
+      img: require('../assets/plant5.png'),
+      about:
+        'Dracaena is a genus of about 120 species of trees and succulent shrubs. The formerly accepted genera Pleomele and Sansevieria are now included in Dracaena. In the APG IV classification system, it is placed in the family Asparagaceae, subfamily Nolinoideae.',
+    },
+    {
+      id: 6,
+      name: 'Swiss cheese plant',
+      price: '120.99',
+      like: false,
+      img: require('../assets/plant6.png'),
+      about:
+        'Monstera deliciosa, the Swiss cheese plant or split-leaf philodendron is a species of flowering plant native to tropical forests of southern Mexico, south to Panama. It has been introduced to many tropical areas, and has become a mildly invasive species in Hawaii, Seychelles, Ascension Island and the Society Islands.',
+    },
+
+    {
+      id: 7,
+      name: 'Aloe vera Plant',
+      price:'86.88',
+      like:false,
+      img: require('../assets/plant7.png'),
+      about:
+      'Aloe vera is a succulent plant species of the genus Aloe. Having some 500 species, Aloe is widely distributed, and is considered an invasive species in many world regions. An evergreen perennial, it originates from the Arabian Peninsula, but grows wild in tropical, semi-tropical, and arid climates around the world.',
+    },
+
+    {
+      id: 8,
+      name: 'Money Plant',
+      price:'75.16',
+      like:false,
+      img: require('../assets/plant8.png'),
+      about:
+      'Money plant is a popular houseplant with immense benefits. Keeping money plant in home is also considered to bring good luck and prosperity. Among the main benefits of money plant is its ability to purify the air by eliminating airborne pollutants like carbon monoxide.',
+    },
+  ];
+  
+  export default plants;
+  
